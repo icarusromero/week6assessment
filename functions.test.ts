4dac4a0df7eddf45847b64250afc2d5bfa6cbc62
Icarus Romero <cloudkingdom07@yahoo.com>
2021-11-20 19:33:33 -0600

    added two passed tests to functions.test.ts that check that shuffleArray returns an array and that the array returned is shuffled

--- functions.test.ts
+++ functions.test.ts
@@ -2,4 +2,14 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    test('return an array', () => {
+        let result = shuffleArray(['fjldsf', 'jfeiof', 'hfuehf', 'huegfywf'])
+        expect(Array.isArray(result)).toBe(true)
+    })
+
+    test('return an array that is the length of the the argument sent', () => {
+        let arr = [1, 2, 3, 4, 5, 6]
+        let result = shuffleArray(arr)
+        expect(result).not.toBe(arr)
+    })
 })
