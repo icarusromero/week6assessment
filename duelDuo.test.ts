7a2bd9240bd5ecafee8a4a9faf7319b013dc31e6
Icarus Romero <cloudkingdom07@yahoo.com>
2021-11-20 20:24:59 -0600

    added two tests to duelDuo.test.ts, only the first one works

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,5 +1,6 @@
 
 import { Builder, Capabilities, By } from "selenium-webdriver"
+import { titleContains } from "selenium-webdriver/lib/until"
 
 require('chromedriver')
 
@@ -17,4 +18,22 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+})
+
+describe('button tests', () => {
+    test('clicking draw button should display div with the id=choices', async () => {
+        await driver.findElement(By.xpath('//button[text()="Draw"]')).click()
+        const choices = await driver.findElement(By.id('choices'))
+        const displayed = await choices.isDisplayed()
+        expect(displayed).toBe(true)
+    })
+
+    // test('clicking add to duo button should display div with the id=player-id', async () => {
+    //     await driver.findElement(By.xpath('//button[text()="Draw"]')).click()
+    //     await driver.findElement(By.xpath('(//button[text()="Add to Duo"])[1]')).click()
+    //     const playerID = await driver.findElement(By.id('player-id'))
+    //     const displayed = await playerID.isDisplayed()
+    //     expect(displayed).toBe(true)
+    // })
+
 })
